81c26805be30bd3425aba41fe36496f2250fe3f4
Mahat J <cyclopsraider@live.co.uk>
2023-11-13 05:58:37 +0000

    Added text boxes and output Window

--- PLE23/Form1.Designer.cs
+++ PLE23/Form1.Designer.cs
@@ -28,12 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            programWindow = new TextBox();
+            commandLine = new TextBox();
+            outputWindow = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)outputWindow).BeginInit();
+            SuspendLayout();
+            // 
+            // programWindow
+            // 
+            programWindow.Location = new Point(35, 32);
+            programWindow.Multiline = true;
+            programWindow.Name = "programWindow";
+            programWindow.Size = new Size(307, 174);
+            programWindow.TabIndex = 0;
+            // 
+            // commandLine
+            // 
+            commandLine.Location = new Point(35, 236);
+            commandLine.Name = "commandLine";
+            commandLine.Size = new Size(315, 35);
+            commandLine.TabIndex = 1;
+            // 
+            // outputWindow
+            // 
+            outputWindow.Location = new Point(388, 32);
+            outputWindow.Name = "outputWindow";
+            outputWindow.Size = new Size(400, 400);
+            outputWindow.TabIndex = 2;
+            outputWindow.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(outputWindow);
+            Controls.Add(commandLine);
+            Controls.Add(programWindow);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)outputWindow).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox programWindow;
+        private TextBox commandLine;
+        private PictureBox outputWindow;
     }
 }
